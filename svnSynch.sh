2136b1d534c13b77ff50c5e72910dd07895bf98f
Piotr Podsiadly <Piotr.Podsiadly@sabre.com>
2011-02-26 14:59:18 +0100

    Doing it simple. Instead of checking in every change as different user all changes will be checked in as GIT user. Checkin messages should contain real commiter to be able to find him or her

--- svnSynch.sh
+++ svnSynch.sh
@@ -9,12 +9,7 @@
 # (this one takes 12-100 hours, so maybe run in background - 2nd one)
 # git-svn show-ignore > .gitignore
 #
-# Set this variable to point to svn.authorsfile
-authorsfile=/opt/portal/git/users.txt
-# The file format of thisone is me@mydomain.com mysvnuser
-# single space separates email from username in each line
-#
-# Note that you will need to path git-svn scipt wherever you have it installed.
+# Note that you will need to patch git-svn scipt wherever you have it installed.
 # It is a perls script so no worries. Find "require Term::ReadKey;"
 # and comment lines with # until Term::ReadKey::ReadMode('restore');
 # then set my $password = ''; or whatever value you need for given user
@@ -30,7 +25,7 @@ authorsfile=/opt/portal/git/users.txt
 git-svn fetch
 
 svnChanges=`git log --pretty="%h" master..git-svn`
-gitChanges=`git log --pretty="%H_%ae" --reverse git-svn..master`
+gitChanges=`git log --pretty="%h" --reverse git-svn..master`
 
 if [ -n $svnChanges -o -n  $gitChanges] ; then
   # Dont accept pushes for a while
@@ -39,26 +34,11 @@ if [ -n $svnChanges -o -n  $gitChanges] ; then
   sleep 30
   # Required on non bare repositories that allows pushing changes
   git reset --hard
-  if [ -n $svnChanges ] ; then
-    # Put git changes on top of SVN recent changes
-    git-svn rebase
-  fi
-  # For each commit that is locally and not yet in SVN
-  for line in $gitChanges ; do
-    hash=${line:0:40}
-    email=${line:41}
-
-    # Mark given checkin as tmp branch
-    # git branch -d tmp
-    # git branch tmp $hash
-    git merge --ff-only $hash || {
-      message=`git log --pretty="%s" $hash^..$hash`
-      # If fast forward fails try regular merge
-      git merge -m "[git] $message" -s resolve $hash
-    }
-    user=`grep -i "$email" $authorsfile|cut -d= -f1`
-    git-svn dcommit --username=$user
-  done
+  # Put git changes on top of SVN recent changes
+  git-svn rebase
+  # Commit all Git changes to SVN with single user.
+  # ---> Ask Git commiters to put theit name in commit message
+  git-svn dcommit --username=piotrp
   # Continue accepting pushes
   git config receive.denyCurrentBranch "ignore"
 else
